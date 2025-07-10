e8952e948482e922a6d687721e8ed90c13aa784c
Jacek Kurlit <jacek.kurlit@kitopi.com>
2025-07-10 16:23:04 +0200

    pipielines update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -27,5 +28,9 @@ export default defineConfig(async () => ({
       // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
   },
 }));
